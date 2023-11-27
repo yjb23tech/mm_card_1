4b1c0953c616b1c4c02b0b73ca5679871059522d
yjb23tech <yjb23tech@gmail.com>
2023-11-27 18:23:14 +0000

    Life without CSS is awful

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,9 +1,20 @@
 import album_art from "../images/lord_willin.jpeg"
+import phone_icon from "../images/phone-icon.png"
+import mail_icon from "../images/mail-icon.png"
 
 const Card = () => {
     return (
         <div>
             <img src={album_art} />
+            <h3>Lord Willing</h3>
+            <div>
+                <img src={phone_icon} />
+                <p>(971) 050 149 4823</p>
+            </div>
+            <div>
+                <img src={mail_icon} />
+                <p>clipse@interscoperecords.com</p>
+            </div>
         </div>
     )
 }
